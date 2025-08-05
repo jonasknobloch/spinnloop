1676cf14f9f87acb74484bdc8aa231e0f7fcb11c
Jonas Knobloch <jonas.knobloch@t-online.de>
2025-08-05 13:31:04 +0200

    WIP

---
+++ tutorial/run.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import pytimeloop.timeloopfe.v4 as tl
+
+def run():
+    spec = tl.Specification.from_yaml_files(
+        "architecture2.yaml",
+        "problem.yaml",
+        "mapping.yaml",
+        "variables.yaml",
+        "intmac.yaml",
+    )
+
+    tl.call_model(spec, output_dir="out")
+
+run()
