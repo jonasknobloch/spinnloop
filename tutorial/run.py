efe7559321fa0b0ff9373af2e273afb5aaa69c19
Jonas Knobloch <jonas.knobloch@t-online.de>
2025-08-19 15:05:52 +0200

    WIP

--- tutorial/run.py
+++ tutorial/run.py
@@ -6,12 +6,12 @@ def run():
     spec = tl.Specification.from_yaml_files(
         "architecture2.yaml",
         "problem.yaml",
-        "mapping.yaml",
+        # "mapping.yaml",
         "variables.yaml",
         "intmac.yaml",
     )
 
-    tl.call_model(spec, output_dir="out")
-    # tl.call_mapper(spec, output_dir="out_mapping")
+    # tl.call_model(spec, output_dir="out")
+    tl.call_mapper(spec, output_dir="out_mapping")
 
 run()
